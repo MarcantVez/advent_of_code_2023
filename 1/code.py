003ebf0a046baa32a75a71dca6b07d1371251ee2
Marc-Antoine Vézina <mvezina@samasource.org>
2023-12-04 14:46:11 -0500

    finish day 1

--- 1/code.py
+++ 1/code.py
@@ -1,13 +1,12 @@
 import re 
 import logging
 
-logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(filename='example.log', level=logging.DEBUG)
 
 calib_sum = 0
 lines = []
 
 digit_map = {
-    'zero': '0',
     'one': '1',
     'two': '2',
     'three': '3',
@@ -25,10 +24,13 @@ with open('input.txt', 'r') as f:
 for i, line in enumerate(lines):
     logging.debug(f"Line {i}: {line}")
     # find all single digits numbers and digit spelled with letters in the line
-    found_digits = re.findall(r'[0-9]|one|two|three|four|five|six|seven|eight|nine', line)
+    found_digits = re.findall(r'(?=(one|two|three|four|five|six|seven|eight|nine|\d))', line.rstrip())
     # convert all found digits to int
     found_digits = [digit_map[digit] if digit in digit_map else digit for digit in found_digits]
-    logging.debug(found_digits)
-    calib_sum += int(f"{found_digits[0]}{found_digits[-1]}")
+    logging.debug(f"Found digits: {found_digits}")
+    # make 2 digit number from first and last digit
+    value = int(f"{found_digits[0]}{found_digits[-1]}")
+    logging.debug(f"Value: {value}\n\n")
+    calib_sum += value
 
 print(calib_sum)
